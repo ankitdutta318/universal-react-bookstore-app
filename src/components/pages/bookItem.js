627df510d40f658aa99a528363a9d02b907c017a
cruxbreaker <geekankit318@gmail.com>
2017-10-26 10:35:14 +0530

    Added BookItem component

---
+++ src/components/pages/bookItem.js
@@ -0,0 +1,22 @@
+"use strict"
+
+import React from 'react';
+import {Row, Col, Well, Button} from 'react-bootstrap';
+
+class BookItem extends React.Component {
+    render() {
+        return(
+            <Well>
+                <Row>
+                    <Col xs={12}>
+                        <h6>{this.props.title}</h6>
+                        <p>{this.props.description}</p>
+                        <h6>usd. {this.props.price}</h6>
+                        <Button bsStyle='primary'>Buy now</Button>
+                    </Col>
+                </Row>
+            </Well>
+        )
+    }
+}
+export default BookItem;
--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -2,9 +2,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {Grid, Row, Button} from 'react-bootstrap';
+import {Grid, Col, Row, Button} from 'react-bootstrap';
+
 import {getBooks} from '../../actions/booksActions';
-import {BookItem} from './bookItem';
+import BookItem from './bookItem';
 
 class BooksList extends React.Component {
     componentDidMount() {
@@ -15,7 +16,7 @@ class BooksList extends React.Component {
         const booksList = this.props.books.map(function(booksArr) {
             return (
                 <Col xs={12} sm={6} md={4} key={booksArr.id}>
-                    <BookItem/>
+                    <BookItem id= {booksArr.id} title={booksArr.title} description={booksArr.description} price={booksArr.price}/>
                 </Col>
             )
         });
