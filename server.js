b40f249b89951da22139bbb661ee32271a9593d8
cruxbreaker <geekankit318@gmail.com>
2017-10-28 19:23:40 +0530

    Added React Router

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 app.use(express.static('public'));
 
 // SET PATH FOR STATIC FILES
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
+// REACT-ROUTER
+import {Router, Route, IndexRoute, browserHistory, hashHistory} from 'react-router';
 
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
@@ -18,17 +20,24 @@ const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
 import BooksList from './components/pages/booksList';
-import Menu from './components/menu';
-import Footer from './components/footer';
+import Cart from './components/pages/cart';
+import BooksForm from './components/pages/booksFrom';
+import Main from './main';
+
+const Routes = (
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={Main}>
+                <IndexRoute component={BooksList}/>
+                <Route path="/admin" component={BooksForm}/>
+                <Route path="/cart" component={Cart}/>
+            </Route>
+        </Router>
+    </Provider>
+);
 
 render(
-    <Provider store={store}>
-        <div>
-            <Menu />
-            <BooksList />
-            <Footer />
-        </div>
-    </Provider>, document.getElementById('app')
+    Routes, document.getElementById('app')
 );
 
 // POST a book
