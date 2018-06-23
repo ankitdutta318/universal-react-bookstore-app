76eafd93d80608d528302b5981a480d94e4e91d4
cruxbreaker <geekankit318@gmail.com>
2018-06-24 01:06:18 +0530

    Added env MONGODB_URI

--- server/config/index.js
+++ server/config/index.js
@@ -5,12 +5,12 @@ let env = process.env.NODE_ENV || 'development';
 if (env ===  'development') {
   process.env.PORT = 3000;
   process.env.API_PORT = 3001;
-  process.env.MONGODB_URI  = 'mongodb://cruxbreaker:ashushaan318@ds117111.mlab.com:17111/universal-react-bookstore';
+  process.env.MONGODB_URI  = process.env.MONGODB_URI || 'mongodb://cruxbreaker:ashushaan318@ds117111.mlab.com:17111/universal-react-bookstore';
 }
 else if (env === 'test'){
   process.env.PORT = 3000;
   process.env.API_PORT = 3001;    
-  process.env.MONGODB_URI  = 'mongodb://cruxbreaker:ashushaan318@ds117111.mlab.com:17111/universal-react-bookstore';
+  process.env.MONGODB_URI  = process.env.MONGODB_URI || 'mongodb://cruxbreaker:ashushaan318@ds117111.mlab.com:17111/universal-react-bookstore';
 }
 
 
